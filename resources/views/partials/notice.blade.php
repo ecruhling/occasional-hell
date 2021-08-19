62a592f06924ece6fdd4145d69284a1a16edbf54
ecruhling <erik@resourceatlanta.com>
2021-08-19 11:26:27 -0500

    change notice; again

--- resources/views/partials/notice.blade.php
+++ resources/views/partials/notice.blade.php
@@ -1,3 +1,3 @@
 <div class="fixed px-4 py-3 bg-red-900 bg-opacity-50 text-center text-sm bottom-0 right-0 z-10 text-white pointer-events-none" id="notice">
-  Site is under redesign.<br>Design is questionable.
+  Site is under redesign.<br>Quality is questionable.
 </div>
