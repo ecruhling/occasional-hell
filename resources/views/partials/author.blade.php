f3f0dc49cbe8d600f633031b5566b1172b64437b
ecruhling <erik@resourceatlanta.com>
2021-08-19 14:18:31 -0500

    add Disturbance font

--- resources/views/partials/author.blade.php
+++ resources/views/partials/author.blade.php
@@ -1,3 +1,3 @@
 <p class="byline author vcard inline-block bg-black text-white text-xs px-2 rounded-lg tracking-widest">
-  <span> {{ __('posted by ', 'sage') }}</span><a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn text-white">{{ get_the_author() }}</a>
+  <span> {{ __('posted by ', 'sage') }}</span><span rel="author" class="fn text-white">{{ get_the_author() }}</span>
 </p>
--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,15 +1,16 @@
 <article @php(post_class())>
   <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
+    <h2 class="entry-title inline-block relative z-10 bg-gradient-to-r from-yellow-200 to-yellow-200 py-1 px-2">
+      <a class="" href="{{ get_permalink() }}">
         {!! $title !!}
       </a>
     </h2>
-
-    @includeWhen(get_post_type() === 'post', 'partials/entry-meta')
+    @include('partials.date-posted')
   </header>
-
   <div class="entry-summary">
     @php(the_excerpt())
   </div>
+  <footer>
+    @include('partials.author')
+  </footer>
 </article>
