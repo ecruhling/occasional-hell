2293898884d50b8935202e04fc8afbfaf7b0a9e6
ecruhling <erik@resourceatlanta.com>
2021-08-20 17:19:05 -0500

    more fonts

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,7 +1,7 @@
 <article @php(post_class())>
   <header>
     <h2 class="entry-title inline-block relative z-10 bg-gradient-to-r from-yellow-200 to-yellow-200 py-1 px-2 mb-0">
-      <a class="font-extrabold" href="{{ get_permalink() }}">
+      <a class="" href="{{ get_permalink() }}">
         {!! $title !!}
       </a>
     </h2>
